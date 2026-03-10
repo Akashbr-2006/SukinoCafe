d5777d3c512cdca796d352e36c3340197839477c
Akash B R <akash.b.renuka@gmail.com>
2026-03-10 12:16:03 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react' // or whatever framework you use
 
 export default defineConfig({
   plugins: [react()],
-  
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+  base: '/SukinoCafe/', // Add this line! It must match your repo name.
+})
